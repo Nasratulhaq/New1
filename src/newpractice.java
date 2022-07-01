1af3c3c9ec79de1dc3ec9bfab3e93648492dfb2c
Mama <105181118+Nasratulhaq@users.noreply.github.com>
2022-07-01 16:16:21 -0600

    please pull the new changes
    Hi..

--- src/newpractice.java
+++ src/newpractice.java
@@ -4,6 +4,7 @@ public class newpractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi this is pulling");
+		// hello this my last code edited by Fazli
 
 	}
 
