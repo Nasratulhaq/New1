d76a670c6c76756eb2015230b1b14e770c330dc2
Nasratulhaq <nasrtulhaq.fazli@tekschool.us>
2022-07-01 15:39:58 -0600

    first commit

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -4,7 +4,7 @@ public class GitHubClass {
 	public static void main(String[] args) {
 		System.out.println("Hi this meee again");
 		System.out.println("Hi this meee again on friday");
-
+		System.out.println("Hi this meee again on friday last practices");
 	}
 
 }
