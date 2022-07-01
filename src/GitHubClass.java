46fc79b26c890f4dc0c1c7f3c9e7d42894748806
Nasratulhaq <nasrtulhaq.fazli@tekschool.us>
2022-06-30 20:10:05 -0600

    2nd comment

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -2,14 +2,8 @@
 public class GitHubClass {
 
 	public static void main(String[] args) {
-		System.out.println("this is the new thig to print");
-		System.out.println("this is the new thig to print");
-		System.out.println("this is the new thig to print");
-		System.out.println("this is the new thig to print");
-		System.out.println("this is the new thig to print");
-		System.out.println("this is the new thig to print");
-		System.out.println("this is the new thig to print");
-		System.out.println("this is the new thig to print");
+		System.out.println("8 pm time");
+		
 
 	}
 
